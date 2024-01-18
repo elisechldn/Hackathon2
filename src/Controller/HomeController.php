705e9d6b975f348de513943903304a62f5d4a44b
elisechldn <elise.chaldjian@gmail.com>
2024-01-18 15:26:04 +0100

    Essai d'include dans la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\WeatherApi;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(WeatherApi $weatherApi): Response
     {
+
+        $latitude = 42.3478;
+        $longitude = -71.0466;
+
+        $weatherData = $weatherApi->getWeather($latitude, $longitude);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'weather'=> $weatherData,
+        ]);
+    }
+
+    public function showWeather(WeatherAPI $weatherApi)
+    {
+        $latitude = 42.3478;
+        $longitude = -71.0466;
+
+        $weatherData = $weatherApi-> getWeather($latitude, $longitude);
+        return $this->render('includes/_weatherwidget.html.twig', [
+            'weather' => $weatherData,
         ]);
     }
 }
--- src/Service/WeatherApi.php
+++ src/Service/WeatherApi.php
@@ -6,20 +6,30 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class WeatherApi
 {
-    public function getWeather(HttpClientInterface $client) //pas de retour
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getWeather(float $latitude, float $longitude)
     {
         //Obligé de précise "client" car le méthode request()
         //appartient à HttpClientInterface et pas à WeatherApi
-        $response = $client->request(
+        $response = $this->client->request(
             'GET',
             'https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=siQzSnOjLXySbo0W28znF17vdZ5lQyGe',
         );
-        return $response->getContent();
+
+        $weatherData= $response->toArray();
+        $temperatureCelcius =$this->fahrenheitToCelcius($weatherData['timelines']['daily'][0]['values']['temperatureAvg']);
+        return $temperatureCelcius;
     }
 
-    public function fahrenheitToCelcius(int $celcius):int
+    public function fahrenheitToCelcius(float $fahrenheit):float
     {
-        $fahrenheit = 0;
+
         $celcius = ($fahrenheit - 32)/1.8;
 
         return $celcius;
