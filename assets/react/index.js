68874d0e64bbbdce784fa3a7850c38388e81f321
Yazzz28 <yacinemennaa@gmail.com>
2024-01-18 16:33:46 +0100

    add react

---
+++ assets/react/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GradientCover from './pages/Card'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <GradientCover/>
+    </React.StrictMode>,
+)
