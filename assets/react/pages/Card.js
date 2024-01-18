7a4a308a94a7909dcbf0c359d0e84c6f9a292389
Dorian Danilet <danilet.dorian@hotmail.fr>
2024-01-18 17:42:22 +0100

    fixe yarn local

--- assets/react/pages/Card.js
+++ assets/react/pages/Card.js
@@ -4,7 +4,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import './styles/app.css';
 
 export default function GradientCover() {
   return (
