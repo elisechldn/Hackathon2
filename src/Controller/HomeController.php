584e5e7654f1c62fe518eafb1765133caa96600c
Yazzz28 <yacinemennaa@gmail.com>
2024-01-19 03:20:47 +0100

    page daily-routine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,26 +10,26 @@ use App\Service\WeatherApi;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(WeatherApi $weatherApi): Response
+    public function index(/*WeatherApi $weatherApi*/): Response
     {
-
+/*
         $latitude = 43.42519;
         $longitude = 6.768337;
 
         $weatherData = $weatherApi->getWeather($latitude, $longitude);
-
+*/
         return $this->render('base.html.twig', [
             'controller_name' => 'HomeController',
-            'weather'=> $weatherData,
+            /*'weather'=> $weatherData,*/
         ]);
     }
 
-    public function showWeather(WeatherAPI $weatherApi)
+    /*public function showWeather(WeatherAPI $weatherApi)
     {
         $latitude = 43.42519;
         $longitude = 6.768337;
 
         $weatherData = $weatherApi-> getWeather($latitude, $longitude);
         return $this->render('includes/_weatherwidget.html.twig');
-    }
+    }*/
 }
